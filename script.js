3cec5bbf71edbe28796df1c184d9b51ad5dc057e
poorvigraphics <poorvigraphics@gmail.com>
2026-07-04 16:00:33 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.gallery img').forEach(img=>{
-  img.addEventListener('click',()=>{
-    const w=window.open('','_blank');
-    w.document.write(`<title>Poorvi Graphics</title><body style="margin:0;background:#111;display:flex;align-items:center;justify-content:center;min-height:100vh"><img src="${img.src}" style="max-width:96%;max-height:96vh;border-radius:12px"></body>`);
-  });
-});
